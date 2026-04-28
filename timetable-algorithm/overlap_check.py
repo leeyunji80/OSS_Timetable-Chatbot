26abe6dbadfd31b58564a96cb4d6c6387f8a9313
BLUEMOON <chaelyn1027@gmail.com>
2026-04-29 00:34:41 +0900

    Add : 강의 정보를 담는 Course 클래스 정의

--- timetable-algorithm/overlap_check.py
+++ timetable-algorithm/overlap_check.py
@@ -1,3 +1,11 @@
+class Course:
+    def __init__(self, name, day, start, end):
+        self.name = name #교과목 명
+        self.day = day
+        self.start = start
+        self.end = end
 
+    def __repr__(self):
+        return f"{self.name}({self.day} {self.start}-{self.end})"
     
 
