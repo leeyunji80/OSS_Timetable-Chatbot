8f5e9ebff74c220b4157985de5f193c81c8d86ac
BLUEMOON <chaelyn1027@gmail.com>
2026-04-29 00:39:51 +0900

    Add : 두 강의 간 시간 겹침 체크 기능 추가

--- timetable-algorithm/overlap_check.py
+++ timetable-algorithm/overlap_check.py
@@ -8,4 +8,9 @@ class Course:
     def __repr__(self):
         return f"{self.name}({self.day} {self.start}-{self.end})"
     
+# 강의 2개가 겹치는 지 확인
+def is_conflict(c1, c2):
+    if c1.day != c2.day:
+        return False
+    return not (c1.end <= c2.start or c2.end <= c1.start)
 
