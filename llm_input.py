01af2cc359cd0681a4176ea6f39e46d9a01d7b00
chaean0318 <yeong22ig@gmail.com>
2026-04-30 15:45:21 +0900

    Add : 시간 및 요일 정보 추출 기능 추가 ( 테스트버전) #6

--- llm_input.py
+++ llm_input.py
@@ -7,4 +7,11 @@ def parse_user_input(text):
         "시간": None
     }
 
+    if "월요일" in processed_text:
+        result["요일"] = "월요일"
+
+    if "오전" in processed_text:
+        result["시간"] = "오전"
+
+    
     return result
