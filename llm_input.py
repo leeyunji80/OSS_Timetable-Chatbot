f31c7d0f99cea6aeba707183c25488a39ae030c9
chaean0318 <yeong22ig@gmail.com>
2026-05-20 13:08:40 +0900

    feat:복합 조건 슬롯 분리 기능 추가  (#27)

--- llm_input.py
+++ llm_input.py
@@ -1,6 +1,4 @@
-def parse_user_input(text):
-    text = text.lower()
-    processed_text = text.strip()
+def parse_single_input(text):
 
     result = {
         "요일": None,
@@ -13,37 +11,60 @@ def parse_user_input(text):
         "화요일",
         "수요일",
         "목요일",
-        "금요일",
-        ]
-
-    for day in days:
-        if day in processed_text:
-            result["요일"] = day
+        "금요일"
+    ]
 
     times = [
         "오전",
         "오후",
-        "아침"]
+        "아침"
+    ]
 
-    for time in times:
-        if time in processed_text:
-            result["시간"] = time
-
-    
     conditions = {
-        "피하고 싶어": "회피",
+        "피하고": "회피",
         "싫어": "회피",
-        "듣고 싶어": "선호",
+        "듣고": "선호",
         "원해": "선호"
     }
-    
+
+    for day in days:
+        if day in text:
+            result["요일"] = day
+
+    for time in times:
+        if time in text:
+            result["시간"] = time
+
     for keyword, value in conditions.items():
-        if keyword in processed_text:
+        if keyword in text:
             result["조건"] = value
-    
+
     return result
 
 
-user_input = "월요일 오전 수업 피하고 싶어"
-    
+def parse_user_input(text):
+
+    split_text = text.split("고")
+
+    results = []
+
+    current_day = None
+
+    for sentence in split_text:
+
+        parsed_result = parse_single_input(sentence)
+
+        if parsed_result["요일"] is not None:
+            current_day = parsed_result["요일"]
+
+        else:
+            parsed_result["요일"] = current_day
+
+        results.append(parsed_result)
+
+    return results
+
+
+user_input = "월요일 오전은 피하고 오후는 듣고 싶어"
+
 print(parse_user_input(user_input))
