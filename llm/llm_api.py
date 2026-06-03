996b0eebadc7ff522b1530992d38ffca66da7477
BLUEMOON <chaelyn1027@gmail.com>
2026-06-03 15:20:43 +0900

    Add: LLM 출력 기능 추가

--- llm/llm_api.py
+++ llm/llm_api.py
@@ -329,7 +329,8 @@ def parse_schedule_text_with_history(session_id: str, user_text: str, api_key: s
             if course.replace(" ", "") in clean_history_text 
         ]
         parsed_data.selected_courses = filtered_courses
-        
+    print(parsed_data.model_dump_json(indent=2))
         # 함수 최종 마무리 반환
     return parsed_data.model_dump_json(indent=2)
+
 
